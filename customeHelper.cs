eeda3596f3f8c4621a9184f47d02b2c63a2d57ca
manish solanki <manish.s@cisinlabs.com>
2023-10-25 18:32:44 +0530

    Add files via upload

---
+++ customeHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace WebApplication5
+{
+    public static class CustomHtmlHelpers
+    {
+
+        public static MvcHtmlString CustomButton(this HtmlHelper htmlHelper, string buttonText, string cssClass)
+        {
+            var buttonTag = new TagBuilder("button");
+            buttonTag.InnerHtml = buttonText;
+            buttonTag.AddCssClass(cssClass);
+            return new MvcHtmlString(buttonTag.ToString());
+        }
+    }
+}
